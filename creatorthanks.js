bbb1f4a6bf69400a365e3a20b117ad03edb38760
samdep5 <72314402+samdep5@users.noreply.github.com>
2021-07-13 16:55:58 -0400

    first draft- minor changes needed

--- creatorthanks.js
+++
@@ -1,2 +0,0 @@
-// JavaScript source code
-function ml_webform_success_4221484() { try { window.top.location.href = "creatorthanks.html" } catch (o) { window.location.href = "creatorthanks.html" } }
